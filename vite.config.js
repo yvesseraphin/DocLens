97468bb06e97254bb33b7ad420ebd9ec36d816fd
MANZI SHIMWA Yves Seraphin <myvesseraphin@gmail.com>
2026-08-08 21:41:03 +0200

    Removing the first bunch of unusable blocks of codes

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    // Serve index.html for all routes so the SPA handles them client-side
     historyApiFallback: true,
   },
 });
